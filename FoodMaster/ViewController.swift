87ac9aa7e51eed81ac64ef7210b963a7677c67fd
sozdemir17 <73183223+sozdemir17@users.noreply.github.com>
2022-12-21 20:05:55 +0300

    Initial Commit

---
+++ FoodMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodMaster
+//
+//  Created by Simay Özdemir on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
